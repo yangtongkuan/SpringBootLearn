612a92e2917b4995ceb69eda75cc31fc608532f6
yangtongkuan <yangtongkuan@qq.com>
2020-01-09 20:22:35 +0800

    ### springboot 集成mybatis 注解版

--- spring-boot-09-mybatis-xml/src/main/java/com/stu/SpringBoot09MybatisXmlApplication.java
+++ spring-boot-09-mybatis-xml/src/main/java/com/stu/SpringBoot09MybatisXmlApplication.java
@@ -1,9 +1,12 @@
 package com.stu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 使用 mapperScan 或者 在maper上标注@Mapper
+// @MapperScan(value = "com.stu.dao")
 public class SpringBoot09MybatisXmlApplication {
 
     public static void main(String[] args) {
--- spring-boot-09-mybatis-xml/src/main/java/com/stu/dao/PersonMapper.java
+++ spring-boot-09-mybatis-xml/src/main/java/com/stu/dao/PersonMapper.java
@@ -3,6 +3,7 @@ package com.stu.dao;
 import com.stu.entity.Person;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
  * @To change this template use File | Settings | File Templates.
  */
 @Mapper
+@Repository
 public interface PersonMapper {
 
     List<Person> queryAll();
